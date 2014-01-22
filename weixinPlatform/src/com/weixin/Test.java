9adb6812fab2e17799094e7f2e09da4979d02207
JINLONG-LIU <aloneb410@163.com>
2014-01-22 11:26:25 +0800

    t

---
+++ weixinPlatform/src/com/weixin/Test.java
@@ -0,0 +1,13 @@
+package com.weixin;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我是一个中国人!");
+		System.out.println("我是一个中国人!");
+	}
+}
